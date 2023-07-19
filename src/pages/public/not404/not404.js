9cb28456001dfedf083ee02e4ebc3b6ca27f7046
amagnies <adrien.magnies@hotmail.fr>
2023-07-19 16:23:04 +0200

    Intégration de la page 404

--- src/pages/public/not404/not404.js
+++ src/pages/public/not404/not404.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Not404 = () => {
     return (
         <section className="not404">
-            Erreur 404 !
+            <h1>404<span>Oups ! La page que vous demandez n'existe pas.</span></h1>
+            <NavLink to="/home" className="navlink__404">Retourner sur la page d'accueil</NavLink>
         </section>
     )
     
