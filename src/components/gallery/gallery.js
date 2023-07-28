6fc68a7b7db27d0b0d03a79aa9d11cc55a029c20
amagnies <adrien.magnies@hotmail.fr>
2023-07-28 08:25:39 +0200

    Ajout condition pour gérer le cas d'une gallery n'ayant qu'une photo

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -15,19 +15,34 @@ const Gallery = ({ slides }) => {
         setIndex(index === 0 ? slides.length - 1 : index - 1)
     }
 
-    return (
-        <div className="slideshow">
-            <img className="slideshow__arrowLeft" src={IconLeft} alt="flèche gauche" onClick={() => previousImage()} />
-            <div className="slideshow__slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
-                {slides.map((picture, index) => (
-                    <div className="slideshow__slide" key={index} style={{ background: `no-repeat center/cover url(${picture})` }}></div>
+    if (slides.length > 1) {
+        return (
+            <div className="slideshow">
+                <img className="slideshow__arrowLeft" src={IconLeft} alt="flèche gauche" onClick={() => previousImage()} />
+                <div className="slideshow__slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+                    {slides.map((picture, index) => (
+                        <div className="slideshow__slide" key={index} style={{ background: `no-repeat center/cover url(${picture})` }}></div>
+    
+                    ))}
+                </div>
+                <img className="slideshow__arrowRight" src={IconRight} alt="flèche droite" onClick={() => nextImage()} />
+                <span className="slideshow__paging"> {index + 1} / {slides.length} </span>
+        </div>
+        );
+    } else {
+        return (
+            <div className="slideshow">
+                <div className="slideshow__slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+                    {slides.map((picture, index) => (
+                        <div className="slideshow__slide" key={index} style={{ background: `no-repeat center/cover url(${picture})` }}></div>
+    
+                    ))}
+                </div>
+        </div>
+        );
+    }
 
-                ))}
-            </div>
-            <img className="slideshow__arrowRight" src={IconRight} alt="flèche droite" onClick={() => nextImage()} />
-            <span className="slideshow__paging"> {index + 1} / {slides.length} </span>
-    </div>
-    );
+    
 };
 
 export default Gallery;
